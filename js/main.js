b001742bfd9f8ae1a9c590db1f62612c563b6041
Hargam <hargam.tdc@gmail.com>
2023-03-28 09:35:00 +0200

    best scores

--- js/main.js
+++ js/main.js
@@ -29,11 +29,11 @@ btnRoll.addEventListener('click', function () {
 
   //3.switch player
   if (dice === 1) {
-    switchPalyer();
+    switchPlayer();
     return;
   }
 
-  //3.dispaly current score
+  //4.dispaly current score
   currentScore += dice;
   if (currentPlayer === 1) {
     currentScoreEL1.textContent = currentScore;
@@ -73,23 +73,28 @@ btnNew.addEventListener('click', () => {
 
 const winner = (score) => {
   if (score >= 20) {
-    //end game
+    let winnerName = '';
     if (currentPlayer === 1) {
       player1EL.classList.add('winner');
+      winnerName = prompt('Joueur 1 a gagné! Entrez votre nom:');
     } else {
       player2EL.classList.add('winner');
+      winnerName = prompt('Joueur 2 a gagné! Entrez votre nom:');
     }
     currentScoreEL1.textContent = 0;
     currentScoreEL2.textContent = 0;
     diceEL.src = '';
     play = false;
+
+    // Mettre à jour les scores affichés
+    showHighScores();
   } else {
     //switch player
-    switchPalyer();
+    switchPlayer();
   }
 };
 
-const switchPalyer = () => {
+const switchPlayer = () => {
   currentPlayer = currentPlayer === 1 ? 2 : 1;
   currentScore = 0;
   currentScoreEL1.textContent = 0;
@@ -97,3 +102,34 @@ const switchPalyer = () => {
   player1EL.classList.toggle('active');
   player2EL.classList.toggle('active');
 };
+
+const showHighScores = () => {
+  // Récupérer les scores stockés localement
+  let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+  // Trier les scores par ordre décroissant
+  highScores.sort((a, b) => b.score - a.score);
+
+  // Afficher les 5 meilleurs scores
+  const highScoresList = document.querySelector('#high-scores');
+  highScoresList.innerHTML = '';
+  for (let i = 0; i < 5 && i < highScores.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = `${highScores[i].name}: ${highScores[i].score}`;
+    highScoresList.appendChild(li);
+  }
+};
+
+// Mise à jour des scores au rechargement
+window.addEventListener('load', showHighScores);
+
+// Faire apparaître et disparaître les meilleurs scores
+const btnHighScores = document.querySelector('#btn--high-scores');
+btnHighScores.addEventListener('click', () => {
+  const bestScoreEL = document.querySelector('.high-content');
+  if (bestScoreEL.classList.contains('hidden')) {
+    bestScoreEL.classList.remove('hidden');
+  } else {
+    bestScoreEL.classList.add('hidden');
+  }
+});
